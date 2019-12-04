d9a9cc94c376edb407412573ae14d4b4084d855a
codyarose <crose992@gmail.com>
2019-12-04 17:43:53 -0600

    Refactored MaterialsContainer to list updated buttons based on materials file

--- src/containers/MaterialsContainer.tsx
+++ src/containers/MaterialsContainer.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Button } from '../components/Button'
 import { useRecipeValue } from '../components/Context'
+import { materials } from '../materials'
+
+const foods = materials.food
 
 export const MaterialsContainer = () => {
 	const { data, updateState, resetState } = useRecipeValue()
 
-	const disabled = data.ingredients >= 5 ? true : false
+	const disabled = data.ingredients === 5 && true
+
+	const listFood = foods.map(food =>
+		<Button
+			key={food.id}
+			disabled={disabled}
+			onClick={() => updateState(food.id)}
+		>
+			{food.name}
+		</Button>
+	)
 
 	return (
 		<>
-			<Button disabled={disabled} price={3} hearts={0.5} duration={0} onClick={updateState}>apple</Button>
-			<Button disabled={disabled} price={5} hearts={0.5} duration={20} onClick={updateState}>banana</Button>
 			<Button onClick={resetState}>reset</Button>
+			{listFood}
 		</>
 	)
 }
