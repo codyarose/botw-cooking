4bfcd6687b96b809f548e2f3ee243ff9c114316a
codyarose <crose992@gmail.com>
2019-12-05 13:30:20 -0600

    Moved parseBuffTime from Context to it's own file in /utils

---
+++ src/utils/parseBuffTime.ts
@@ -0,0 +1,19 @@
+// Returns the amount of extra time the ingredient gives based on it's
+// buff type
+export const parseBuffTime = (type: any) => {
+	if (type === null) return 0
+	switch (type) {
+		case 'attack':
+		case 'defense':
+			return 20
+		case 'cold resist':
+		case 'heat resist':
+		case 'shock resist':
+		case 'fireproof':
+			return 120
+		case 'speed':
+			return 30
+		case 'stealth':
+			return 90
+	}
+}
