e718e1d2aa2e5ccb9854e7e853b8d0e1ba86e477
codyarose <crose992@gmail.com>
2019-12-20 13:38:21 -0600

    Refactored findFirsts to use IMaterial

--- src/utils/findFirsts.ts
+++ src/utils/findFirsts.ts
@@ -1,12 +1,14 @@
+import { IMaterial } from './interfaces'
+
 const removeDuplicates = (array: Array<object>) => (
 	array.filter((element: any, index: number) =>
 		array.indexOf(element) === index
 	)
 )
 
-export const findFirsts = (array: object[], term: any) => {
+export const findFirsts = (array: IMaterial[], term: any) => {
 	// Filters array for only ingredients that have the 'first' key
-	const hasFirstKey = array.filter((element: any) => element.first !== null)
+	const hasFirstKey = array.filter((element) => element.first !== null)
 	// Filters hasFirstKey by term
 	const filterByTerm = hasFirstKey.filter((element: any) => ((element || {}).first || {})[term])
 	// Removes duplicate ingredients because the extra time only applies to
