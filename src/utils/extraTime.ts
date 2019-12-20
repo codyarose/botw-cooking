8a83364d8c5006ce862f6733edc6bd8040009fe6
codyarose <crose992@gmail.com>
2019-12-20 13:38:43 -0600

    Refactored extraTime to use IMaterial and reduce

--- src/utils/extraTime.ts
+++ src/utils/extraTime.ts
@@ -1,36 +1,31 @@
 import { parseBuffTime } from './parseBuffTime'
 import { findFirsts } from './findFirsts'
+import { IMaterial } from './interfaces'
 
 // Returns the extra time from ingredients that give extra time
 // on the first occurrence of the ingredient
-const extraTimeFromFirst = (array: Array<object>, baseTime: number) => {
+const extraTimeFromFirst = (array: IMaterial[]) => {
 	const uniqueArray = findFirsts(array, 'duration')
-
-	// Gets the `first.duration` value from each ingredient and adds them
-	// to the baseTime parameter
-	uniqueArray.forEach((element: any) => {
-		const fromFirst = ((element || {}).first || {}).duration
-		baseTime += fromFirst
-	})
-
-	return baseTime
+	// Gets sum of the `first.duration` value from each ingredient
+	return uniqueArray.length && uniqueArray.reduce((acc, item: any) => acc + item.first.duration, 0)
 }
 
 // Returns the extra time from ingredients that have a buff
-const extraTimeFromBuff = (array: Array<object>, baseTime: number) => {
+const extraTimeFromBuff = (array: IMaterial[]) => {
 	// Runs parseBuffTime on each ingredients `buff.type` and adds the returned
 	// value to the baseTime parameter
+	let result = 0
 	array.forEach((element: any) => {
 		const fromBuff = parseBuffTime(((element || {}).buff || {}).type)
-		baseTime += fromBuff
+		result += fromBuff
 	})
-	return baseTime
+	return result
 }
 
 // Returns total extra time from all ingredients
-export const extraTime = (array: Array<object>) => {
+export const extraTime = (array: IMaterial[]) => {
 	let time = 0
-	const fromBuff = extraTimeFromBuff(array, time)
-	const fromFirst = extraTimeFromFirst(array, time)
+	const fromBuff = extraTimeFromBuff(array)
+	const fromFirst = extraTimeFromFirst(array)
 	return fromBuff + fromFirst
 }
