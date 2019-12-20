a977066b110788f233a188fdb91bb3b51c8834de
codyarose <crose992@gmail.com>
2019-12-20 12:23:08 -0600

    Added readout for buff and potency to OutputContainer

--- src/containers/OutputContainer.tsx
+++ src/containers/OutputContainer.tsx
@@ -5,7 +5,7 @@ import { useRecipeValue } from '../components/Context'
 import { secToMin } from '../utils/secToMin'
 
 export const OutputContainer = () => {
-	const { ingredients, time, hearts, price } = useRecipeValue()
+	const { ingredients, time, hearts, price, buff } = useRecipeValue()
 
 	return (
 		<>
@@ -13,6 +13,8 @@ export const OutputContainer = () => {
 			<div>Price: {price}</div>
 			<HeartsContainer amount={hearts} />
 			<div>Time: {time && secToMin(time)}</div>
+			<div>Buff: {buff && buff.type}</div>
+			<div>Potency: {buff && buff.potency}</div>
 		</>
 	)
 }
