5ba6c736b73f3144ade5236869552dc7a1b660af
codyarose <crose992@gmail.com>
2019-12-20 10:23:29 -0600

    Refactored HeartsContainer to use Heart type and repeatHearts into reusable function

--- src/containers/HeartsContainer.tsx
+++ src/containers/HeartsContainer.tsx
@@ -12,16 +12,16 @@ export const HeartsContainer: SFC<IHearts> = ({ amount }) => {
 	const renderedWholeHearts = wholeHearts > 6 ? (wholeHearts - abbrHearts) : wholeHearts
 	const partialHeart = amount % 1
 
-	const repeatHearts = [...Array(renderedWholeHearts)].map((_, i) => {
-		return <Heart key={i} size={1} />
-	})
+	const repeatHearts = (count: number, type: string) => {
+		return [...Array(count)].map((_, i) => <Heart key={i} size={1} type={type} />)
+	}
 
 	return (
 		<StyledHeartsWrap>
 			Hearts:
-			{amount > 6 && <Heart size={1} abbr={abbrHearts} />}
-			{amount > 0 && repeatHearts}
-			{partialHeart > 0 && <Heart size={partialHeart} />}
+			{amount > 6 && <Heart size={1} abbr={abbrHearts} type={'normal'} />}
+			{amount > 0 && repeatHearts(renderedWholeHearts, 'normal')}
+			{partialHeart > 0 && <Heart size={partialHeart} type={'normal'} />}
 		</StyledHeartsWrap>
 	)
 }
