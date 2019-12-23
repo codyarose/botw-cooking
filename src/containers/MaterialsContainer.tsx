96da8c2f56b10cc97b380c260568f88c95b4fe56
codyarose <crose992@gmail.com>
2019-12-23 14:31:23 -0600

    Removed comments

--- src/containers/MaterialsContainer.tsx
+++ src/containers/MaterialsContainer.tsx
@@ -4,8 +4,6 @@ import { Button } from 'components/Button'
 import { useRecipeValue } from 'components/Context'
 import { materials } from 'materials'
 
-// const materialsArray: IMaterials = materials
-
 export const MaterialsContainer = () => {
 	const { ingredients, updateIngredients, resetState } = useRecipeValue()
 
@@ -21,55 +19,11 @@ export const MaterialsContainer = () => {
 			{item.name}
 		</Button>
 	)
-	// const listPlants = materials.plants.map(item =>
-	// 	<Button
-	// 		key={item.id}
-	// 		disabled={disabled}
-	// 		onClick={() => updateIngredients!(item.id)}
-	// 	>
-	// 		<img src={require(`./images/${item.id}.png`)} alt={item.name} />
-	// 		{item.name}
-	// 	</Button>
-	// )
-	// const listCritters = materials.critters.map(item =>
-	// 	<Button
-	// 		key={item.id}
-	// 		disabled={disabled}
-	// 		onClick={() => updateIngredients!(item.id)}
-	// 	>
-	// 		<img src={require(`./images/${item.id}.png`)} alt={item.name} />
-	// 		{item.name}
-	// 	</Button>
-	// )
-	// const listMinerals = materials.minerals.map(item =>
-	// 	<Button
-	// 		key={item.id}
-	// 		disabled={disabled}
-	// 		onClick={() => updateIngredients!(item.id)}
-	// 	>
-	// 		<img src={require(`./images/${item.id}.png`)} alt={item.name} />
-	// 		{item.name}
-	// 	</Button>
-	// )
-	// const listMonsters = materials.monster.map(item =>
-	// 	<Button
-	// 		key={item.id}
-	// 		disabled={disabled}
-	// 		onClick={() => updateIngredients!(item.id)}
-	// 	>
-	// 		<img src={require(`./images/${item.id}.png`)} alt={item.name} />
-	// 		{item.name}
-	// 	</Button>
-	// )
 
 	return (
 		<StyledMaterialsContainer>
 			<Button onClick={resetState}>reset</Button>
 			{listFood}
-			{/* {listPlants}
-			{listCritters}
-			{listMinerals}
-			{listMonsters} */}
 		</StyledMaterialsContainer>
 	)
 }
