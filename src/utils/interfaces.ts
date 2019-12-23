5645ba978e18e1bc878d53cf2acef1e6a12efd14
codyarose <crose992@gmail.com>
2019-12-23 11:46:39 -0600

    Refactored IFirst structure

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,19 +1,17 @@
+interface IFirst {
+	duration: number
+	hearts: number
+}
+
 export interface IMaterial {
 	id: string
 	name: string
 	price: number
 	hearts: number
-	first: null | {
-		[key: string]: IFirst
-	}
+	first: null | IFirst
 	buff: {
 		type: string
 		potency: number
 	}
 	locations: string[]
-}
-
-interface IFirst {
-	duration: number
-	hearts: number
 }
