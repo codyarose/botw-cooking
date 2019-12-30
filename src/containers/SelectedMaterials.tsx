bd8bc9fd92a6a035f096c04e31fdd963ade286c3
codyarose <crose992@gmail.com>
2019-12-30 11:03:54 -0600

    Added SelectedMaterials container styles

--- src/containers/SelectedMaterials.tsx
+++ src/containers/SelectedMaterials.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useRecipeValue } from 'components/Context'
 import { Button } from 'components/Button'
 import { IMaterial } from 'utils/interfaces'
@@ -16,8 +17,16 @@ export const SelectedMaterials = ({ selected }: any) => {
 		</Button>
 	)
 	return (
-		<div>
-			Ingredients: {listIngredients}
-		</div>
+		<StyledSelectedMaterials>
+			{listIngredients}
+		</StyledSelectedMaterials>
 	)
 }
+
+const StyledSelectedMaterials = styled.div`
+	--auto-grid-min-size: 9rem;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(var(--auto-grid-min-size), 1fr));
+	gap: 2rem;
+	padding-bottom: 1.5rem;
+`
