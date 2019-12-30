7969b7f7cc7a80272bc7e5d7c42e99ee9f30551d
codyarose <crose992@gmail.com>
2019-12-30 10:57:17 -0600

    Added Roboto with webfontloader

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader'
+
+WebFont.load({
+	google: {
+		families: ['Roboto:400,500,500italic', 'sans-serif']
+	}
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
