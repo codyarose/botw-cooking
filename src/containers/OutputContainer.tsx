3291dbcfb945efe49623b367f3b883e8c326519d
codyarose <crose992@gmail.com>
2019-12-30 13:27:58 -0600

    Added StyledOutputText to OutputContainer

--- src/containers/OutputContainer.tsx
+++ src/containers/OutputContainer.tsx
@@ -11,16 +11,39 @@ export const OutputContainer = () => {
 
 	return (
 		<StyledOutputContainer>
-			<SelectedMaterials selected={ingredients} />
-			<PriceContainer price={price || 0} />
-			<HeartsContainer amount={hearts || 0} />
-			<div>Time: {time && secToMin(time)}</div>
-			<div>Buff: {buff && buff.type}</div>
-			<div>Potency: {buff && buff.potency}</div>
+			{<SelectedMaterials selected={ingredients} />}
+			<StyledOutputTextContainer>
+				<PriceContainer price={price || 0} />
+				<HeartsContainer amount={hearts || 0} />
+				<div>Time: {time && secToMin(time)}</div>
+				<div>Buff: {buff && buff.type}</div>
+				<div>Potency: {buff && buff.potency}</div>
+			</StyledOutputTextContainer>
 		</StyledOutputContainer>
 	)
 }
 
 const StyledOutputContainer = styled.div`
-	padding: 1.5rem 3rem;
+	max-width: 64rem;
+	margin: 0 auto;
+	padding: 3rem 3rem 1.5rem;
+`
+
+const StyledOutputTextContainer = styled.div`
+	position: relative;
+	background-color: rgba(0,0,0,0.5);
+	border: 3px solid transparent;
+	padding: .5rem;
+	line-height: 1.5;
+	&::before {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: calc(100% + 10px);
+		height: calc(100% + 10px);
+		border: 3px solid rgba(0,0,0,0.5);
+		border-radius: 4px;
+	}
 `
