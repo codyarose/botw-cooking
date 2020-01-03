cff73a5ea598df0127fa72a87d0dfd5f88e73d04
codyarose <crose992@gmail.com>
2020-01-03 13:17:58 -0600

    Added space to 'Full Recovery'

--- src/containers/HeartsContainer.tsx
+++ src/containers/HeartsContainer.tsx
@@ -24,7 +24,7 @@ export const HeartsContainer: SFC<IHearts> = ({ amount }) => {
 
 	return (
 		<StyledHeartsWrap>
-			{heartsType === 'temp' && 'Full Recovery'}
+			{heartsType === 'temp' && 'Full Recovery '}
 			{heartsCount > 6 && <Heart size={1} abbr={abbrHearts} type={heartsType} />}
 			{heartsCount > 0 && repeatHearts(renderedWholeHearts, heartsType)}
 			{partialHeart > 0 && <Heart size={partialHeart} type={heartsType} />}
