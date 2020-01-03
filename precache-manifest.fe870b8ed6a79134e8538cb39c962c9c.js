c2e85df3893b2b8642fb4d006b838e101fd51c37
codyarose <crose992@gmail.com>
2020-01-03 13:52:54 -0600

    Updates

---
+++ precache-manifest.fe870b8ed6a79134e8538cb39c962c9c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db927eaa402d679be83267a6458e48dc",
+    "url": "/botw-cooking/index.html"
+  },
+  {
+    "revision": "3c5852fbaf85210a90f2",
+    "url": "/botw-cooking/static/js/2.cc765489.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/botw-cooking/static/js/2.cc765489.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0264fcec70f33310e467",
+    "url": "/botw-cooking/static/js/main.dacd81be.chunk.js"
+  },
+  {
+    "revision": "75a1acf62c49c02b13e9",
+    "url": "/botw-cooking/static/js/runtime-main.1feb0bc8.js"
+  },
+  {
+    "revision": "45aeb37dc420ca254e71f822b416b070",
+    "url": "/botw-cooking/static/media/chilly.45aeb37d.svg"
+  },
+  {
+    "revision": "33aedffda1c705c86d2852397a32fee1",
+    "url": "/botw-cooking/static/media/electro.33aedffd.svg"
+  },
+  {
+    "revision": "1f43d0f25653a2c59d3e7ca72834faac",
+    "url": "/botw-cooking/static/media/fireproof.1f43d0f2.svg"
+  },
+  {
+    "revision": "5f0938d2a9d8d910b0be71ee9e4b9800",
+    "url": "/botw-cooking/static/media/hasty.5f0938d2.svg"
+  },
+  {
+    "revision": "0de86ef791e39faf87f407f423fdbf5a",
+    "url": "/botw-cooking/static/media/mighty.0de86ef7.svg"
+  },
+  {
+    "revision": "5cec611cf99137f8c1e004245d04e8d7",
+    "url": "/botw-cooking/static/media/sneaky.5cec611c.svg"
+  },
+  {
+    "revision": "3f3eb5e146629bb23a52cac5243159f4",
+    "url": "/botw-cooking/static/media/spicy.3f3eb5e1.svg"
+  },
+  {
+    "revision": "52003d24b86234407c673df556c43b71",
+    "url": "/botw-cooking/static/media/tough.52003d24.svg"
+  }
+]);
