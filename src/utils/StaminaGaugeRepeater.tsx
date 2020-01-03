dac95175c5af5a0bab8b663220b1a9cdb8aa25b8
codyarose <crose992@gmail.com>
2020-01-03 13:40:12 -0600

    Added formula for enduras to StaminaGaugeRepeater

--- src/utils/StaminaGaugeRepeater.tsx
+++ src/utils/StaminaGaugeRepeater.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { StaminaGauge, IStaminaGauge } from 'components/StaminaGauge'
 
+const minMax = (value: number, min: number, max: number) => {
+	return Math.min(Math.max(value, min), max)
+}
+
 export const StaminaGaugeRepeater = ({ progress, type }: IStaminaGauge) => {
-	const multiplier = progress === 10 ? 1.4 : 1.333
-	const staminaCount = Math.min(Math.max((progress < 3 ? progress : Math.round(progress * multiplier)), 0), 15)
+	let staminaCount = 0
+	if (type === 'stamina') {
+		const multiplier = progress === 10 ? 1.4 : 1.333
+		staminaCount = minMax(progress < 3 ? progress : Math.round(progress * multiplier), 0, 15)
+	} else if (type === 'enduras') {
+		staminaCount = minMax(progress < 4 ? 1 : Math.floor(progress / 2), 0, 10)
+	}
 	const wholeGauges = Math.floor(staminaCount / 5)
 	const partialGauge = staminaCount % 5
 	const repeatGauge = () => {
