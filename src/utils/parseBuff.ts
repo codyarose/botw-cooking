8c3729832e407c9a3cfb88c7f9a0a388766667ea
codyarose <crose992@gmail.com>
2020-01-05 13:23:15 -0600

    Refactored parseBuff to fix TS flag

--- src/utils/parseBuff.ts
+++ src/utils/parseBuff.ts
@@ -4,9 +4,9 @@ export const parseBuff = (array: IMaterial[]) => {
 	// Array of buff values for current ingredients
 	const buffs = array.map((item: IMaterial) => (item || {}).buff)
 	// Removes any that have 'none' buff type
-	const nonesRemoved = buffs.filter(({ type }) => type !== 'none')
+	const nonesRemoved = buffs.filter(buff => buff!.type !== 'none')
 	// Array of the buff types as strings, duplicates removed
-	const buffTypes = Array.from(new Set(nonesRemoved.map(({ type }) => type)))
+	const buffTypes = Array.from(new Set(nonesRemoved.map(buff => buff!.type)))
 	// If multiple buff types are added they'll cancel each other
 	// in-game and no buff will be added
 	if (buffTypes.length > 1) return [{ type: 'none', potency: 0 }]
