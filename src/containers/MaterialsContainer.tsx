1faa7753fa7627bb967bbad7a41816e098205a7d
codyarose <crose992@gmail.com>
2020-01-06 09:36:07 -0600

    Added locations tooltip to material buttons and updated LazyLoad with placeholder

--- src/containers/MaterialsContainer.tsx
+++ src/containers/MaterialsContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import LazyLoad from 'react-lazyload'
+import Tooltip from 'react-simple-tooltip'
 import { Button } from 'components/Button'
 import { useRecipeValue } from 'components/Context'
 import { materials } from 'materials'
@@ -10,25 +11,43 @@ export const MaterialsContainer = () => {
 
 	const disabled = ingredients && ingredients.length === 5 && true
 
+	const placeholderStyle = {
+		height: '100px'
+	};
+
 	const listFood = materials.map(item =>
-		<Button
+		<Tooltip
 			key={item.id}
-			disabled={disabled}
-			onClick={() => updateIngredients!(item.id)}
+			content={item.locations.join(', ')}
+			placement="bottom"
+			customCss={css`
+				white-space: nowrap;
+			`}
+			background="rgba(0,0,0,0.75)"
+			fadeDuration={500}
+			fadeEasing="ease-in-out"
+			fontSize="12px"
+			padding={8}
+			radius={4}
 		>
-			<LazyLoad
-				once={true}
-				height={100}
-				offset={100}
+			<Button
+				disabled={disabled}
+				onClick={() => updateIngredients!(item.id)}
 			>
-				<picture>
-					<source type="image/webp" srcSet={`images/${item.id}.webp`} />
-					<source type="image/png" srcSet={`images/${item.id}.png`} />
-					<img src={`images/${item.id}.png`} alt={item.name} />
-				</picture>
-			</LazyLoad>
-			<span className="material__name">{item.name}</span>
-		</Button>
+				<LazyLoad
+					once={true}
+					offset={100}
+					placeholder={<div style={placeholderStyle} />}
+				>
+					<picture>
+						<source type="image/webp" srcSet={`images/${item.id}.webp`} />
+						<source type="image/png" srcSet={`images/${item.id}.png`} />
+						<img src={`images/${item.id}.png`} alt={item.name} />
+					</picture>
+				</LazyLoad>
+				<span className="material__name">{item.name}</span>
+			</Button >
+		</Tooltip >
 	)
 
 	return (
