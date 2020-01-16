3d241aa1763cb83f1c47a98f3ec181dc23ff7c8d
codyarose <crose992@gmail.com>
2020-01-16 15:55:59 -0600

    Added FilterContainer component and filterMaterials util

--- src/containers/FiltersContainer.tsx
+++ src/containers/FiltersContainer.tsx
@@ -1,21 +1,51 @@
-import React, { ChangeEvent } from 'react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import { useRecipeValue } from 'components/Context'
+import { Radio } from 'components/Radio'
 
-interface Props {
+export const FiltersContainer: React.FC = () => {
+	const { handleFilter } = useRecipeValue()
 
-}
+	const [term, setTerm] = useState("")
+	const [category, setCategory] = useState("")
+	const categoryOptions = [
+		'all',
+		'general',
+		'hearty',
+		'sneaky',
+		'hasty',
+		'tough',
+		'energizing',
+		'chilly',
+		'enduring',
+		'mighty',
+		'spicy',
+		'electro',
+		'fireproof',
+		'monster'
+	]
 
-export const FiltersContainer: React.FC<Props> = () => {
-	const { filter, handleFilter } = useRecipeValue()
+	const handleTermChange = (e: ChangeEvent<HTMLInputElement>) => setTerm(e.target.value)
 
-	const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
-		handleFilter!(e.target.value)
-	}
+	const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => setCategory(e.target.value)
+
+	useEffect(() => {
+		handleFilter!(term, category)
+	}, [term, category])
 
 	return (
 		<form>
-			<input onChange={updateFilter} type="text" />
-			{filter}
+			<input onChange={handleTermChange} type="text" />
+			<div>
+				{categoryOptions.map(option =>
+					<Radio
+						group="category"
+						value={option}
+						label={option.charAt(0).toUpperCase() + option.slice(1)}
+						onChange={handleRadioChange}
+						checked={category === option}
+					/>
+				)}
+			</div>
 		</form>
 	)
 }
---
+++ src/utils/filterMaterials.ts
@@ -0,0 +1,13 @@
+import { materials } from 'materials'
+
+export const filterMaterials = (term: string, key: any, category: string) => {
+	const termPattern = new RegExp(term, 'i')
+	const categoryPattern = new RegExp(category, 'i')
+
+	const filteredByTerm = materials.filter((obj: any) => termPattern.test(obj[key]))
+	const filteredByCategory = filteredByTerm.filter((obj: any) => categoryPattern.test(obj['tags']))
+
+	console.log(filteredByCategory)
+
+	return category === 'all' ? filteredByTerm : filteredByCategory
+}
